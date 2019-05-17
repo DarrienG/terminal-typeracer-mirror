3476353d45c7dd330d33cac40567587d7b6d90b6
DarrienG <darrienglasser@outlook.com>
2019-05-17 07:10:40 -0400

    Basic user input
    Does not validate or take in passages to read.
    Sizing is also a little weird too. Size should be played around with.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{stdin, stdout, Error, Write};
-use termion::cursor::Goto;
+use termion::cursor::{Left, Right};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -13,61 +13,74 @@ use unicode_width::UnicodeWidthStr;
 
 // TODO: Calculate constraints based on terminal size
 // e.g. smaller terminal means smaller padding on top and bottom
+fn get_bounds() -> [Constraint; 4] {
+    [
+        Constraint::Percentage(20),
+        Constraint::Percentage(30),
+        Constraint::Percentage(30),
+        Constraint::Percentage(20),
+    ]
+}
+
+// TODO: Read in passage from somewhere
+fn get_passage() -> String {
+    "The quick brown fox jumps over the lazy dog".to_owned()
+}
 
 fn main() -> Result<(), Error> {
     let stdout = stdout().into_raw_mode()?;
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
+    let screen = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(screen);
     let mut terminal = Terminal::new(backend)?;
 
     let mut user_input = String::new();
+    let mut raw_passage = get_passage();
 
     loop {
         let stdin = stdin();
+
         terminal
             .draw(|mut f| {
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .margin(10)
-                    .constraints(
-                        [
-                            Constraint::Percentage(20),
-                            Constraint::Percentage(30),
-                            Constraint::Percentage(30),
-                            Constraint::Percentage(20),
-                        ]
-                        .as_ref(),
-                    )
+                    .constraints(get_bounds().as_ref())
                     .split(f.size());
-                Block::default()
-                    .title("The text passage be here")
+                let passage_block = Block::default()
                     .borders(Borders::ALL)
+                    .title_style(Style::default());
+                Paragraph::new([Text::raw(&raw_passage)].iter())
+                    .block(passage_block.clone().title("Passage to type"))
+                    .alignment(Alignment::Left)
                     .render(&mut f, chunks[2]);
 
-                let block = Block::default()
+                let typing_block = Block::default()
                     .borders(Borders::ALL)
                     .title_style(Style::default().modifier(Modifier::BOLD));
                 Paragraph::new([Text::raw(&user_input)].iter())
-                    .block(block.clone().title("Fellas type here"))
+                    .block(typing_block.clone().title("Fellas type here"))
                     .alignment(Alignment::Left)
                     .render(&mut f, chunks[3]);
             })
             .unwrap();
-
-        write!(
-            terminal.backend_mut(),
-            "{}",
-            Goto(4 + user_input.width() as u16, 4)
-        )?;
-
         for c in stdin.keys() {
-            let key_code = c.unwrap();
-            if key_code == Key::Ctrl('c') {
-                return Ok(());
-            } else if key_code == Key::Backspace {
-                user_input.pop().unwrap();
-            } else {
-                //user_input.push(key_code(k));
+            match c.unwrap() {
+                Key::Ctrl('c') => return Ok(()),
+                Key::Backspace => {
+                    user_input.pop();
+                    if user_input.chars().count() > 0 {
+                        write!(terminal.backend_mut(), "{}", Left(1))?;
+                    }
+                    break;
+                }
+                Key::Char(c) => {
+                    user_input.push(c);
+                    write!(terminal.backend_mut(), "{}", Right(1))?;
+                    break;
+                }
+                _ => {
+                    break;
+                }
             }
         }
     }
