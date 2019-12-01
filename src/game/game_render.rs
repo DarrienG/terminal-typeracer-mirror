89efb3793de90260c63ffb019676133303f503a6
DarrienG <darrienglasser@outlook.com>
2019-12-01 13:04:13 -0500

    Add missing comma in debug text

--- src/game/game_render.rs
+++ src/game/game_render.rs
@@ -26,7 +26,7 @@ pub struct GameState<'a> {
 
 impl<'a> GameState<'a> {
     fn get_debug_output(&self) -> String {
-        format!("Running with options:\n Legacy WPM: {},  word_idx: {},  start: {}\nUser has err: {}  instant_death_enabled: {}\npassage_path: {}\ncurrent_word: {}",
+        format!("Running with options:\n Legacy WPM: {},  word_idx: {},  start: {}\nUser has err: {},  instant_death_enabled: {}\npassage_path: {}\ncurrent_word: {}",
                 self.stats.get_legacy_wpm(),
                 self.word_idx,
                 self.stats.get_start_time(),
