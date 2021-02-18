319bf941c015dd2b3cd14a948878a7fa2e8baf7a
DarrienG <darrien@glasser.dev>
2021-02-17 19:07:39 -0500

    Force refinery version
    A minor version bump seems to have broken compiles for us. This sets us
    on the version that does not break.
    
    Will fix this in the future, but I want a version that compiles now out
    first.

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn debug_enabled_default() -> bool {
 const VERSION: &str = "DEBUG";
 
 #[cfg(not(debug_assertions))]
-const VERSION: &str = "2.0.4";
+const VERSION: &str = "2.0.5";
 
 fn main() -> Result<(), Error> {
     let typeracer_config = config::get_config();
