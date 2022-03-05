24ff47c88b8656849c0fac84252a33c2ee2f5cda
darrieng <darrien@glasser.dev>
2022-03-05 10:38:19 -0500

    Prep typeracer for new release

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn debug_enabled_default() -> bool {
 const VERSION: &str = "DEBUG";
 
 #[cfg(not(debug_assertions))]
-const VERSION: &str = "2.0.12";
+const VERSION: &str = "2.0.13";
 
 fn main() -> Result<(), Error> {
     let typeracer_config = config::get_config();
